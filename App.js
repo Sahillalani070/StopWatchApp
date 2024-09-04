d9f58ffcc6f7063ba4c04419fa8f5c4ab2a18eff
Sahil Lalani <69220306+Sahillalani070@users.noreply.github.com>
2024-09-04 14:48:36 -0400

    Stopwatch app first commit: All the pre requisits are ready. Only front end is required now.

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState, useRef, useCallback } from 'react';
 
 export default function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setRunning] = useState(false);
+  const [result, setResult] = useState([]);
+  const timer = useRef(null);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -18,3 +23,83 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+// Calculation and presentation:
+const padToTwo = (number) => (number <= 9 ? `0${number}` : number); // used to pad in the format 00:00:00
+export const displaytime = (centisec) => {
+  let min = 0;
+  let sec = 0;
+
+  if (centisec < 0) {
+    centisec = 0;
+  }
+  if (centisec < 100)
+    return `00:00:${padToTwo(centisec)}`;
+
+  let remCentisec = centisec % 100;
+  sec = (centisec - remCentisec) / 100;
+
+  if (sec < 60)
+    return `00:${padToTwo(sec)}: ${padToTwo(remCentisec)}`
+
+  let remSec = sec % 60;
+  min = (sec - remSec) / 60;
+  return `${padToTwo(min)}:${padToTwo(remSec)}: ${padToTwo(remCentisec)}`
+}
+// Button Controls:
+function Stopwatch() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setRunning] = useState(false);
+  const [result, setResult] = useState([]);
+  const timer = useRef(null);
+
+  const handleLeftPress = useCallback(() => {
+    setTime(0);
+  }, [time]);
+  const handleCenterPress = useCallback(() => {
+    if (!isRunning) {
+      const interval = setInterval(() => {
+        setTime((previousTime) => previousTime + 1);
+      }, 10);
+      timer.current = interval
+      setRunning(true);
+    }
+  }, [isRunning]);
+  const handleRightPress = useCallback(() => {
+    clearInterval(timer.current);
+    setRunning(false);
+  }, [isRunning]);
+}
+function Controls({ isRunning, handleLeftPress, handleCenterPress, handleRightPress }) {
+  return (
+    <>
+      <TouchableOpacity
+        style={[]}
+        onPress={handleLeftPress}>
+        <View>
+          <Text style={{}}>
+            {"Reset"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[]}
+        onPress={handleCenterPress}>
+        <View>
+          <Text style={{}}>
+            {"Start"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[]}
+        onPress={handleRightPress}>
+        <View>
+          <Text style={[]}>
+            {"Stop"}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  )
+}
+//export default React.memo(Controls);
